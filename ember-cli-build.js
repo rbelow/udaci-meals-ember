c8aa02a8762b950258ef58c0db2ac8fbc0642cff
Ruben Below <rbelow@hagafu.com>
2017-05-10 03:51:09 +0200

    fingerprints

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,7 +6,7 @@ module.exports = function(defaults) {
     // Add options here
     // NOTE Update references on build process
     fingerprint: {
-        replaceExtensions: ['html', 'css', 'js']
+        replaceExtensions: ['html', 'hbs', 'css', 'js']
     }
   });
 
